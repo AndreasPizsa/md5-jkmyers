aa6622c992ef6b5db28fabf896ff352a9335865b
Ben <therealbenwiley@gmail.com>
2016-05-30 22:25:06 -0400

    'var' added in front of 'txt' declaration to conform to strict mode.

--- src/md5.js
+++ src/md5.js
@@ -98,7 +98,7 @@ return cmn(c ^ (b | (~d)), a, b, x, s, t);
 }
 
 function md51(s) {
-txt = '';
+var txt = '';
 var n = s.length,
 state = [1732584193, -271733879, -1732584194, 271733878], i;
 for (i=64; i<=s.length; i+=64) {
